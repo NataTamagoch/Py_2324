cb5be6a5510e14a5a57f28a4980390c191a61fd9
natalia <sunshine187103@gmail.com>
2023-11-12 19:25:29 +0300

    Добавила изменения в put_data_to_db.py для нового сервера.

--- put_data_to_db.py
+++ put_data_to_db.py
@@ -1,8 +1,20 @@
 import pymysql
-
-connection = pymysql.connect(host="10.10.103.161", user='pyuser', password='123456', database="test")
-
+f = open("users.txt", "r")
+connection = pymysql.connect(host="nadejnei.net", user='student', password='1q2w#E$R', database="test", port=33306)
 cursor = connection.cursor()
 
-cursor.execute("insert into telsprav values("kirill", "morozov", "m", "89110271345")")
+for line in f:
+    line = line.strip()
+    split_line = line.split(":")
+    name = split_line[0]
+    surname = split_line[1]
+    sex = split_line[2]
+    phone = split_line[3]
+    cursor.execute(f'insert into telsprav values("{name}","{surname}","{sex}","{phone}")')
+f.close()
+
 connection.commit()
+
+
+
+
