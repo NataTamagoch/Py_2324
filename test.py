a86dfa02e955fff3fab6374d16f87b108f9e5976
natalia <sunshine187103@gmail.com>
2023-11-12 16:48:33 +0300

    Добавила файл тест

---
+++ test.py
@@ -0,0 +1,12 @@
+a = range(1, 100)
+users = ["kirill", "ivan", "pavel", "petr"]
+print(type(a))
+print(type(users))
+
+
+for element in a:
+    print(element * 15)
+
+for user in users:
+    #print(user)
+    print(f"Уважаемый {user}, добро дожаловать!!!")
